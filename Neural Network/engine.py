250e98272b15c3dec1e44c9e4f40f0789401201c
Dmitry Ivanov <dmitry.ivanov08202003@gmail.com>
2025-01-10 21:49:21 +0300

    Adding engine, neural net, developing process and changing readme

---
+++ Neural Network/engine.py
@@ -0,0 +1,105 @@
+import math
+""" Stores a single scalar and it's gradint"""
+class Value:
+
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children) # previous values for computation graph
+        self._op = _op # the op that produced this node
+        self.label = label # additional label for visualization
+
+    def __repr__(self):
+        return f'Value(data = {self.data})'
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):  # other multiplied by self
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other ** (-1)
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "for now we can only raise to the power of integer or float value "
+        out = Value(self.data ** other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t ** 2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v): # topological sort
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Neural-Network-Builded-From-Scratch
+# Neural Network Built From Scratch
+
+## An implementation of a tiny neural network library with a PyTorch like API featuring a custom 'Value' class that serves as a lightweight autograd engine. However, it's enough for building deep neural networks for solving the binary classification problem.
+
+## Project structure
+Project consists of the following components:
+1. 'Neural Network/' folder contains implementation of autograd engine and neural network:
+    - 'engine.py' - 'Value' class for representation of numeric values and computing gradients and backward passing.
+    - 'neuralnet.py' - implementation of 'Neuron', 'Layer' and 'MLP' classes for building multilayers perceptrons and working with them.
+
+## Example of usage
+Example of supported operations
+```python
+from engine import Value
+
+a = Value(-4.0)
+b = Value(2.0)
+c = a + b
+d = a * b + b**3
+c += c + 1
+c += 1 + c + (-a)
+d += d * 2 + (b + a).relu()
+d += 3 * d + (b - a).relu()
+e = c - d
+f = e**2
+g = f / 2.0
+g += 10.0 / f
+print(f'{g.data:.4f}') # prints 24.7041, the outcome of this forward pass
+g.backward()
+print(f'{a.grad:.4f}') # prints 138.8338, i.e. the numerical value of dg/da
+print(f'{b.grad:.4f}') # prints 645.5773, i.e. the numerical value of dg/db
+```
+
+## Training a neural network
+The notebook 'demo.ipynb' provides a process of training a multilayer perceptron binary classifier. coming soon
